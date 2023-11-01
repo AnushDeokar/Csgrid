8ee409d61b40332abc7c37b47e6fb5f141528b63
AnushDeokar <deokaranush@gmail.com>
2023-11-01 23:36:28 +0530

    navbar updated

--- src/components/layouts/MainNavbar.tsx
+++ src/components/layouts/MainNavbar.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import NavBarDropDownList from "../ui/NavBarDropdownList";
 import SearchBar from "../ui/SearchBar";
-import Link from "next/link";
+import NavbarSigninBtn from "../ui/NavbarSigninBtn";
+import { currentUser } from "@clerk/nextjs";
 
-function MainNavbar(): React.ReactNode {
+async function MainNavbar(): Promise<any> {
+  const user = await currentUser();
+
   return (
     <div className="hidden md:flex items-center md:px-16 lg:px-24 sticky h-16 border-b justify-between w-screen top-0">
       <div className="font-sans flex items-center justify-between gap-10">
@@ -12,11 +15,7 @@ function MainNavbar(): React.ReactNode {
       </div>
       <div className="flex items-center gap-4">
         <SearchBar />
-        <Link href="/signin">
-          <button className="bg-primary text-primary-foreground shadow hover:bg-primary/90 text-xs py-2 px-4 font-semibold rounded-md">
-            Signin
-          </button>
-        </Link>
+        <NavbarSigninBtn user={user} />
       </div>
     </div>
   );
