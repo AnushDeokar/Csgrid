b8fda7476620fd1a797f7b4978f8fa569254eff6
AnushDeokar <deokaranush@gmail.com>
2023-11-24 19:49:20 +0530

    Blogcard added

---
+++ src/components/ui/BlogCard.tsx
@@ -0,0 +1,26 @@
+"use client";
+import React from "react";
+import { useRouter } from "next/navigation";
+interface BlogCardProps {
+  title: string;
+  url: string;
+  brief: string;
+  date: string;
+}
+
+function BlogCard({ title, url, brief, date }: BlogCardProps): React.ReactNode {
+  const router = useRouter();
+  return (
+    <div
+      className="flex flex-col cursor-pointer gap-2"
+      onClick={() => router.push(url)}
+    >
+      <img src="cs2.png" className="rounded-md w-full" />
+      <h3 className="font-semibold text-2xl">{title}</h3>
+      <p className="text-muted-foreground w-full">{brief} ...</p>
+      <p className="text-left text-muted-foreground mt-4">{date}</p>
+    </div>
+  );
+}
+
+export default BlogCard;
